c9af75711e8a2ab76e80fc881628430514393dd1
Mohd Rahil Khan <rahil.k@fragmatic.io>
2026-02-20 01:01:05 +0530

    added gh-pages

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/mohdrahil-portfolio/",
   plugins: [react()],
   server: {
     port: 3000,
